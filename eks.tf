a35771fbaf465833df66ec20a877250ed888f7e5
Niharika10-oss <78817780+Niharika10-oss@users.noreply.github.com>
2026-03-06 18:53:00 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,48 +1,21 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.1"
+  version = "19.15.3" # Stable version compatible with Provider v5.x
 
-  cluster_name                   = local.name
+  cluster_name    = "niharika-eks-cluster"
+  cluster_version = "1.30"
+
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
-  cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-  }
-
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.intra_subnets
-
-  # EKS Managed Node Group(s)
-  eks_managed_node_group_defaults = {
-    ami_type       = "AL2_x86_64"
-    instance_types = ["m5.large"]
-
-    attach_cluster_primary_security_group = true
-  }
-
   eks_managed_node_groups = {
-      cluster-wg = {
+    nodes = {
       min_size     = 1
-      max_size     = 2
-      desired_size = 1
+      max_size     = 3
+      desired_size = 2
 
-      instance_types = ["t3.large"]
-      capacity_type  = "SPOT"
-
-      tags = {
-        ExtraTag = "helloworld"
-      }
+      instance_types = ["t3.medium"]
     }
   }
-
-  tags = local.tags
 }
