06c95e7183a2e0ce5075cd216d61eff25f36591c
Niharika10-oss <78817780+Niharika10-oss@users.noreply.github.com>
2026-03-06 18:49:08 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,16 +1,27 @@
-locals {
-  region = "us-east-1"
-  name   = "kastro-eks-cluster"
-  vpc_cidr = "10.123.0.0/16"
-  azs      = ["us-east-1a", "us-east-1b"]
-  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
-  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
-  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
-  tags = {
-    Example = local.name
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" 
+    }
   }
 }
 
 provider "aws" {
   region = "us-east-1"
+}
+
+# This allows Terraform to talk to the EKS cluster once it is created
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This ensures the 'aws' command uses the region from above
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
 }
